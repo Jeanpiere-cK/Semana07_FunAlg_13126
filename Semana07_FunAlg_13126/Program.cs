9ccfd716eaa9975b9874e8f2c17827deb7c1a5dd
Jhocep Verastegui <snkje123@gmail.com>
2025-05-07 17:35:24 -0500

    commit final

--- Semana07_FunAlg_13126/Program.cs
+++ Semana07_FunAlg_13126/Program.cs
@@ -13,17 +13,21 @@ namespace Semana07_FunAlg_13126
     {
         static void Main(string[] args)
         {
-            int suma=0, num;
+            NotaAlumno();
+        }   
+        static void NumeroPerfecto()
+        {
+            int suma = 0, num;
             Console.WriteLine("Ingrese el número: ");
             num = int.Parse(Console.ReadLine());
             for (int i = 1; i < num; i++)
             {
-                if(num%i==0)
+                if (num % i == 0)
                 {
                     suma = suma + i;
                 }
             }
-            if(suma==num)
+            if (suma == num)
             {
                 Console.WriteLine("Numero Perfecto");
 
@@ -32,7 +36,64 @@ namespace Semana07_FunAlg_13126
             {
                 Console.WriteLine("El numero no es perfecto");
             }
-            
         }
+        static void NumeroPerfectoWhile()
+        {
+            int num, suma = 0, div;
+            //num=6
+            Console.WriteLine("Ingrese un Número: ");
+            num = int.Parse(Console.ReadLine());
+
+            div = num-1;
+
+            while (div>0)
+            {
+                if(num%div==0)
+                {
+                    suma = suma + div;
+                }
+                div--;
+            }
+            Console.WriteLine("Suma: " + suma);
+            if (suma == num)
+            {
+                Console.WriteLine("Numero Perfecto");
+
+            }
+            else
+            {
+                Console.WriteLine("El numero no es perfecto");
+            }
+            Console.ReadKey();
+
+        }
+        static void NotaAlumno()
+        {
+            int num, suma = 0, div;
+            //num=6
+            Console.WriteLine("Ingrese un Número: ");
+            num = int.Parse(Console.ReadLine());
+
+            div = num - 1;
+            do
+            {
+                if(num%div==0)
+                {
+                    suma = suma + div;
+                }
+                div--;
+            }
+            while (div>0);
+            if (suma == num)
+            {
+                Console.WriteLine("Numero Perfecto");
+
+            }
+            else
+            {
+                Console.WriteLine("El numero no es perfecto");
+            }
+        }
+
     }
 }
