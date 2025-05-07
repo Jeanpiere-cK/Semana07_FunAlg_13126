27c7b0f23c3e98fd6e5bf94f55d3ef35456066b8
Jhocep Verastegui <snkje123@gmail.com>
2025-05-07 15:39:44 -0500

    Agregar archivos de proyecto.

---
+++ Semana07_FunAlg_13126/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana07_FunAlg_13126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma=0, num;
+            Console.WriteLine("Ingrese el número: ");
+            num = int.Parse(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                if(num%i==0)
+                {
+                    suma = suma + i;
+                }
+            }
+            if(suma==num)
+            {
+                Console.WriteLine("Numero Perfecto");
+
+            }
+            else
+            {
+                Console.WriteLine("El numero no es perfecto");
+            }
+            
+        }
+    }
+}
